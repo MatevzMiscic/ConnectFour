72afd4014428efee762a7b1e263d679fbe5f3d97
MatevzMiscic <matevz.miscic@gmail.com>
2020-07-08 23:08:30 +0200

    Popravil sem tekstovni vmesnik

--- interface.py
+++ interface.py
@@ -1,15 +1,23 @@
 import board
+import bot
 
 b = board.Board(7, 6, 4)
-b.print()
+bots = [False, True]
+colors = ["Red", "Blue"]
 
 while b.outcome() == 2:
-    if b.moves % 2 == 0:
-        print("Red's turn: ", end="")
+    column = 0
+    color = b.moves % 2
+    print(colors[color] + "'s turn: ", end="")
+    if bots[color]:
+        column = bot.takeTurn(b, color, 4)
+        print(column + 1)
     else:
-        print("Blue's turn: ", end="")
-    col = int(input())
-    b.play(col - 1)
+        column = int(input()) - 1
+        if column < 0 or column >= b.width or b.ground[column] >= b.height:
+            print("Invalid move.")
+            continue
+    b.play(column)
     b.print()
 
 state = b.outcome()
@@ -17,5 +25,6 @@ if state == 0:
     print("Red player won.")
 if state == 1:
     print("Blue player won.")
+
 if state == 3:
     print("Draw")
