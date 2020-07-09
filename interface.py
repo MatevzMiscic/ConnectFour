d2290935e0503e12aacb273470a66f6b49ceb895
MatevzMiscic <matevz.miscic@gmail.com>
2020-07-09 21:45:28 +0200

    Izboljšal tekstovni vmesnik

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@ import board
 import bot
 
 b = board.Board(7, 6, 4)
-bots = [False, True]
+bots = [True, False]
 colors = ["Red", "Blue"]
 
 def printTraps(b):
@@ -16,14 +16,12 @@ def printTraps(b):
                 if traps[x][y][2 * c + 1] >= 1:
                     print(" semi trap: ", x + 1, y + 1, " count: ", traps[x][y][2 * c + 1])
 
-#start = [4, 4, 4, 4, 4, 4, 5, 3, 6, 7, 5, 3, 5, 5, 5, 5, 3, 3, 3, 3, 7, 2, 7, 1, 7, 7, 7, 1, 2, 2, 2, 6, 2]
 start = []
 for i in start:
-    b.play(i - 1)
-if start != []:
-    b.print()
+    b.play(i)
 
 while b.outcome() == 2:
+    b.print()
     column = 0
     color = b.turns % 2
     print(colors[color] + "'s turn: ", end="")
@@ -31,14 +29,23 @@ while b.outcome() == 2:
         column = bot.takeTurn(b, color, 5)
         print(column + 1)
     else:
-        column = int(input()) - 1
+        command = input()
+        if command == "undo":
+            for i in range(2):
+                if b.turns > 0:
+                    b.undo()
+            continue
+        if not command.isdigit():
+            print("Invalid move.")
+            continue
+        column = int(command) - 1
         if column < 0 or column >= b.width or b.ground[column] >= b.height:
             print("Invalid move.")
             continue
     b.play(column)
-    print("Game value:", bot.evaluate(b))
-    b.print()
+    print("Game value:", round(bot.simpleEvaluate(b), 2), round(bot.evaluate(b), 2))
 
+b.print()
 state = b.outcome()
 if state == 0:
     print("Red player won.")
@@ -46,3 +53,4 @@ if state == 1:
     print("Blue player won.")
 if state == 3:
     print("Draw")
+#print(b.history)
