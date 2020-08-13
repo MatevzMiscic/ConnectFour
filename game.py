d8eb4df25801a6dedc4f28210168f4c9d898b99f
MatevzMiscic <matevz.miscic@gmail.com>
2020-08-13 16:01:23 +0200

    Dodal razred, ki skbi za igro

---
+++ game.py
@@ -0,0 +1,26 @@
+import board
+import bot
+
+class Game:
+    def __init__(self):
+        self.width = 7
+        self.height = 6
+        self.connect = 4
+        self.bots = [False, True]
+        self.grid = board.Board(self.width, self.height, self.connect)
+    
+    def legal(self):
+        return self.connect <= self.width and self.connect <= self.height
+
+    def setBoard(self, width, height, connect):
+        self.width = width
+        self.height = height
+        self.connect = connect
+        self.grid = board.Board(width, height, connect)
+        return self.legal()
+
+    def setFirst(isBot):
+        self.bots[0] = isBot
+    
+    def setSecond(isBot):
+        self.bots[1] = isBot
